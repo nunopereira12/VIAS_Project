e1266df6ba698e4d2341902c460cc67229cf83e1
Ricardo <ricardobamf@gmail.com>
2023-09-08 19:09:13 +0100

    Diagrams update Adicionadas 2 linhas no application properties dadas pelo zé

--- src/main/java/pt/upskill/vias/repositories/UserRepository.java
+++ src/main/java/pt/upskill/vias/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package pt.upskill.vias.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pt.upskill.vias.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByUsername(String username);
 }
