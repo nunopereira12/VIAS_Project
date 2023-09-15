37f8a2b6d840d34185a01a8e1c104c07f13682b0
Ricardo <ricardobamf@gmail.com>
2023-09-15 23:53:54 +0100

    Adição de métodos aos serviços das routes

---
+++ src/main/java/pt/upskill/vias/services/routes/info/LineInfoServiceImpl.java
@@ -0,0 +1,30 @@
+package pt.upskill.vias.services.routes.info;
+
+import org.json.JSONObject;
+
+public class LineInfoServiceImpl implements LineInfoService {
+    @Override
+    public String icon(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String color(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String text_color(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String short_name(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String name(JSONObject obj) {
+        return null;
+    }
+}
---
+++ src/main/java/pt/upskill/vias/services/routes/info/StepInfoServiceImpl.java
@@ -0,0 +1,66 @@
+package pt.upskill.vias.services.routes.info;
+
+import org.json.JSONObject;
+import pt.upskill.vias.models.routes.Step;
+
+public class StepInfoServiceImpl implements StepInfoService {
+    @Override
+    public String distance(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String duration(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String polyline(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String html_instructions(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String travel_mode(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String departure_stop(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String departure_time(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String arrival_stop(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String arrival_time(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String headsign(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public String num_stops(JSONObject obj) {
+        return null;
+    }
+
+    @Override
+    public Step buildStep(JSONObject obj) {
+        return null;
+    }
+}
