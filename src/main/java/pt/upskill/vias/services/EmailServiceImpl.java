654ad80f8c295c6c63e96f1574a9aa36c657f82f
nunop <nuno.pereiraaa12@gmail.com>
2023-09-15 18:29:02 +0100

    EmailServiceImpl! Bug no push anterior

---
+++ src/main/java/pt/upskill/vias/services/EmailServiceImpl.java
@@ -0,0 +1,33 @@
+package pt.upskill.vias.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String htmlBody) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlBody, true);
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+}
